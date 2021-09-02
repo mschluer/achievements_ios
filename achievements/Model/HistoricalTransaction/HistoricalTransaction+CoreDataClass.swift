6d2771681520010d473bb7951da2df7e8909a0be
mschluer <max.schluer@icloud.com>
2021-09-02 15:40:18 +0200

    introduce new entity

---
+++ achievements/Model/HistoricalTransaction/HistoricalTransaction+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  HistoricalTransaction+CoreDataClass.swift
+//  achievements
+//
+//  Created by Maximilian Schluer on 02.09.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(HistoricalTransaction)
+public class HistoricalTransaction: NSManagedObject {
+
+}
---
+++ achievements/Model/HistoricalTransaction/HistoricalTransaction+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  HistoricalTransaction+CoreDataProperties.swift
+//  achievements
+//
+//  Created by Maximilian Schluer on 02.09.21.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension HistoricalTransaction {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<HistoricalTransaction> {
+        return NSFetchRequest<HistoricalTransaction>(entityName: "HistoricalTransaction")
+    }
+
+    @NSManaged public var text: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var amount: Float
+    @NSManaged public var balance: Float
+    @NSManaged public var currentTransaction: AchievementTransaction?
+
+}
+
+extension HistoricalTransaction : Identifiable {
+
+}
